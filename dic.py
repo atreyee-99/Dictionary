40eede730dd2bc362ebf9649287dd7ea31ab8a6f
Atreyee Paul <66584524+atreyee-99@users.noreply.github.com>
2020-07-11 20:20:04 +0530

    Add files via upload

---
+++ dic.py
@@ -0,0 +1,23 @@
+from tkinter import *
+from PyDictionary import PyDictionary
+import json
+
+root=Tk()
+root.geometry('250x200')
+
+#searches the meaning
+def find_meaning():
+    word=e1.get()
+    dic=PyDictionary()
+    l1.config(text=str(dic.meaning(word))) 
+
+e1=Entry(root,font=('times 15',15,'bold'))
+e1.grid(row=2,column=2)
+
+btn=Button(root,text='Find meaning',command=find_meaning)
+btn.grid(row=4,column=2)
+
+l1=Label(root,text="",font='times 10') #space for displaying results
+l1.grid(row=6,column=2)
+
+root.mainloop()
